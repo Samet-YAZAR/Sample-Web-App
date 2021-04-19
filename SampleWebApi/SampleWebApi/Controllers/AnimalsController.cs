c666f2405404debb3019bc54326b3a55374a2948
Samet Yazar <s18378@pjwstk.edu.pl>
2021-04-19 05:30:44 +0200

    Create Controllor with normal and async key words, Create Animal class

---
+++ SampleWebApi/SampleWebApi/Controllers/AnimalsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAnimals()
+        {
+            return Ok();
+        }
+
+        [HttpGet("proc")]
+        public async Task<IActionResult> GetAnimalsByProc()
+        {
+            return Ok();
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> UpdateAnimals()
+        {
+            return Ok();
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAnimals2()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SampleWebApi/SampleWebApi/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
